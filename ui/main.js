e68209c6461e451be487dc6f9c419e652a9f1ba8
shyamsundar1123 <shyamsundar10@gmail.com>
2016-09-30 20:13:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+var img=document.getElementById('img');
+marginLeft=0;
+function moveLeft(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onClick=function() {
+    var interval=setInterval(moveLeft,100);
+    
+}; 
+    
