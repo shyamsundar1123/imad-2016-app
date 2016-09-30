559a0c98496c765bc105f564b5467d2014b16afe
shyamsundar1123 <shyamsundar10@gmail.com>
2016-09-30 20:14:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var img=document.getElementById('img');
-marginLeft=0;
+var marginLeft=0;
 function moveLeft(){
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
