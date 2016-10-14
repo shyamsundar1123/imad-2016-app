5ab60f08bf5fe848031018a4f3fbf3992f2d2c89
shyamsundar1123 <shyamsundar10@gmail.com>
2016-10-14 19:46:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -49,6 +49,6 @@ img.onclick=function() {
         };        
      var nameInput=document.getElementById('name');
     var names=nameInput.value;
-     request.open('GET','http://shyamsundar1123.imad.hasura-app.io/submit-name?name='+name,true);
+     request.open('GET','http://shyamsundar1123.imad.hasura-app.io/submit-name?name='+names,true);
     request.send(null);
     };
