98799cee72cf5fdd03d79c47dedaf5d3f474c4e0
shyamsundar1123 <shyamsundar10@gmail.com>
2016-10-14 12:47:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,4 +10,16 @@ var marginLeft=0;
 img.onclick=function() {
     var interval=setInterval(moveLeft,100);
     }; 
-    
+    var button=document.getElementById('counter');
+    button.onclick=function(){
+        var request=new XMLHttpRequest();
+        request.onreadystatechange=function(){
+            if(request.readyState===XMLHttpRequest.DONE){
+                if(request.status===200){
+                    var counter=request.responseText;
+                    var span=document.getElementById('count');
+                    span.innerHTML=counter.toString();
+                }
+            }
+        };
+    };
