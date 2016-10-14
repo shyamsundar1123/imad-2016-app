8b152e2e222f8741d3f36efb47a68588ee3d2b2e
shyamsundar1123 <shyamsundar10@gmail.com>
2016-10-14 19:54:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
-var names;
+var names=0;
 app.get('/submit-name', function(req,res){
     var name=req.query.name;
     names.push(name);
